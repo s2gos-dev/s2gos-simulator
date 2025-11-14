37e92aed4af3824a4ae1af2bb37439da4c696c07
nicolae marton <nicolae.marton@gmail.com>
2025-11-14 11:21:16 +0100

    Add config settings.

---
+++ src/s2gos_simulator/setting/_settings.py
@@ -0,0 +1,12 @@
+from dynaconf import Validator
+from s2gos_utils.setting import settings as util_settings
+
+# Validate Simulator config
+# util_settings.validators.register(
+#     Validator("simulator.sim_test", must_exist=True), # Add Validators here
+# )
+# util_settings.validators.validate(only="simulator")
+
+
+# Forward s2gos_utils settings
+settings = util_settings
